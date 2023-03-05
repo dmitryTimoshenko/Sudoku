fa556eb31c634ec5587aaae9dd633b075ed6cf9e
Klim <xarkoni@gmail.com>
2023-03-05 16:06:33 +0300

    fullpack qf

--- sudoku.js
+++ sudoku.js
@@ -15,24 +15,24 @@ function solvePuzzle(boardString) {
 
 function getMatrixBoard(str, size) {
   return str.split('').reduce((acc, currentValue, index) => {
-      if (index % size === 0) {
-          acc.push([]);
-      }
-      const lastRowIndex = acc.length - 1;
+    if (index % size === 0) {
+      acc.push([]);
+    }
+    const lastRowIndex = acc.length - 1;
 
-      acc[lastRowIndex].push(currentValue)
-      return acc;
+    acc[lastRowIndex].push(currentValue);
+    return acc;
   }, []);
 }
 
 function findEmptyPlace(board) {
   for (let rowIndex = 0; rowIndex < board.length; rowIndex += 1) {
-      const row = board[rowIndex];
-      for (let colIndex = 0; colIndex < row.length; colIndex += 1) {
-          if (board[rowIndex][colIndex] === '-') {
-              return [rowIndex, colIndex];
-          }
+    const row = board[rowIndex];
+    for (let colIndex = 0; colIndex < row.length; colIndex += 1) {
+      if (board[rowIndex][colIndex] === '-') {
+        return [rowIndex, colIndex];
       }
+    }
   }
 
   return null;
@@ -42,96 +42,92 @@ function solve(board, size) {
   const emptyCell = findEmptyPlace(board);
 
   if (emptyCell === null) {
-      return board;
+    return board;
   }
 
   for (let i = 1; i <= size; i += 1) {
-      const possibleValue = String(i);
+    const possibleValue = String(i);
 
-      const [rowIndex, colIndex] = emptyCell;
-      let isValid = true;
+    const [rowIndex, colIndex] = emptyCell;
+    let isValid = true;
 
-      const possibleRowValues = {};
-      const possibleColValues = {};
+    const possibleRowValues = {};
+    const possibleColValues = {};
 
-      for (let j = 0; j < size; j += 1) {
-          if (board[j][colIndex] !== '-') {
-              if (possibleRowValues[board[j][colIndex]]) {
-                  return false;
-              } else {
-                  possibleRowValues[board[j][colIndex]] = true;
-              }
-          }
-          if (board[j][colIndex] === possibleValue && j !== rowIndex) {
-              isValid = false;
-          }
+    for (let j = 0; j < size; j += 1) {
+      if (board[j][colIndex] !== '-') {
+        if (possibleRowValues[board[j][colIndex]]) {
+          return false;
+        } else {
+          possibleRowValues[board[j][colIndex]] = true;
+        }
+      }
+      if (board[j][colIndex] === possibleValue && j !== rowIndex) {
+        isValid = false;
+      }
+    }
+
+    if (!isValid) {
+      continue;
+    }
+
+    for (let j = 0; j < size; j += 1) {
+      if (board[rowIndex][j] !== '-') {
+        if (possibleColValues[board[rowIndex][j]]) {
+          return false;
+        } else {
+          possibleColValues[board[rowIndex][j]] = true;
+        }
       }
 
-      if (!isValid) {
-          continue;
+      if (board[rowIndex][j] === possibleValue && j !== colIndex) {
+        isValid = false;
+      }
+    }
+
+    if (!isValid) {
+      continue;
+    }
+
+    const boxSize = Math.sqrt(size);
+    const startBoxRow = Math.floor(rowIndex / boxSize) * boxSize;
+    const startBoxCol = Math.floor(colIndex / boxSize) * boxSize;
+
+    for (let j = startBoxRow; j < startBoxRow + boxSize; j += 1) {
+      for (let k = startBoxCol; k < startBoxCol + boxSize; k += 1) {
+        if (board[j][k] === possibleValue && j !== rowIndex && k !== colIndex) {
+          isValid = false;
+        }
+      }
+    }
+
+    if (isValid) {
+      board[rowIndex][colIndex] = possibleValue;
+
+      if (solve(board, size)) {
+        return true;
       }
 
-      for (let j = 0; j < size; j += 1) {
-          if (board[rowIndex][j] !== '-') {
-              if (possibleColValues[board[rowIndex][j]]) {
-                  return false;
-              } else {
-                  possibleColValues[board[rowIndex][j]] = true;
-              }
-          }
-
-          if (board[rowIndex][j] === possibleValue && j !== colIndex) {
-              isValid = false;
-          }
-      }
-
-      if (!isValid) {
-          continue;
-      }
-
-      const boxSize = Math.sqrt(size);
-      const startBoxRow = Math.floor(rowIndex / boxSize) * boxSize;
-      const startBoxCol = Math.floor(colIndex / boxSize) * boxSize;
-
-      for (let j = startBoxRow; j < startBoxRow + boxSize; j += 1) {
-          for (let k = startBoxCol; k < startBoxCol + boxSize; k += 1) {
-              if (board[j][k] === possibleValue && j !== rowIndex && k !== colIndex) {
-                  isValid = false;
-              }
-          }
-      }
-
-      if (isValid) {
-          board[rowIndex][colIndex] = possibleValue;
-
-          if (solve(board, size)) {
-              return true;
-          }
-
-          board[rowIndex][colIndex] = '-';
-      }
+      board[rowIndex][colIndex] = '-';
+    }
   }
 
   return false;
 }
 
-
-
-
-
 /**
-* Принимает игровое поле в том формате, в котором его вернули из функции solve.
-* Возвращает булевое значение — решено это игровое поле или нет.
-*/
+ * Принимает игровое поле в том формате, в котором его вернули из функции solve.
+ * Возвращает булевое значение — решено это игровое поле или нет.
+ */
 function isSolved(board) {
   return findEmptyPlace(board) === null;
 }
 
 /**
-* Принимает игровое поле в том формате, в котором его вернули из функции solve.
-* Возвращает строку с игровым полем для последующего вывода в консоль.
-* Подумай, как симпатичнее сформировать эту строку.
-*/
+ * Принимает игровое поле в том формате, в котором его вернули из функции solve.
+ * Возвращает строку с игровым полем для последующего вывода в консоль.
+ * Подумай, как симпатичнее сформировать эту строку.
+ */
 function prettyBoard(board) {
   console.table(board);
 }
